60380367ab00e70240e0e732febb7fb41ed3a77e
vasyaod <vasyaod@difur.ru>
2012-03-09 13:30:32 +0600

    Реализовано событие "клика" при помощи touchstart and endstart.
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- client/src/permguide.js
+++ client/src/permguide.js
@@ -69,12 +69,56 @@
 			}
 			$(this).on("mouseup", newFn);
 		};
+		
+		$.fn.touchclick = function(fn) {
+			
+			var touchclickState = null;
+			
+			var downHandler = function (event)
+			{
+				touchclickState = {
+					x: event.clientX,
+					y: event.clientY
+				}
+			};
+			
+			var upHandler = function (event)
+			{
+				if (touchclickState == null)
+					return;
+				
+				var touchclickStateTmp = touchclickState;
+				touchclickState = null;
+				if (Math.abs(event.clientX - touchclickStateTmp.x) < 10 &&
+					Math.abs(event.clientY - touchclickStateTmp.y) < 10)
+				{
+					fn({
+						type: "touchclick",
+						target: event.target,
+						changedTouches: 
+						[
+							{
+								clientX: event.clientX,
+								clientY: event.clientY,
+								screenX: event.screenX,
+								screenY: event.screenY,
+								pageX: event.pageX,
+								pageY: event.pageY
+							}
+						]
+					});
+				}
+			};
+			
+			$(this).on("mousedown", downHandler);
+			$(this).on("mouseup", upHandler);
+		};
 	} else {
 		// Если тач поддерживается, то используем нужные события.
 		$.fn.touchstart = function(fn) {
 			var newFn = function (event)
 			{
-		//		event.preventDefault();
+				event.preventDefault();
 				fn(event);
 			}
 			$(this).each( function () {
@@ -85,7 +129,7 @@
 		$.fn.touchmove = function(fn) {
 			var newFn = function (event)
 			{
-		//		event.preventDefault();
+				event.preventDefault();
 				fn(event);
 			}
 			$(this).each( function () {
@@ -96,14 +140,42 @@
 		$.fn.touchend = function(fn) {
 			var newFn = function (event)
 			{
-		//		event.preventDefault();
+				event.preventDefault();
 				fn(event);
 			}
 			$(this).each( function () {
 				this.addEventListener("touchend", newFn);
 			});		
+		};	
+
+		$.fn.touchclick = function(fn) {
+			
+			var touchclickState = null;
+			
+			var downHandler = function (event)
+			{
+				event.preventDefault();
+				touchclickState = {
+					x: event.changedTouches[0].clientX,
+					y: event.changedTouches[0].clientY
+				}
+			}
+			
+			var upHandler = function (event)
+			{
+				event.preventDefault();
+				if (touchclickState == null)
+					return;
+				var touchclickStateTmp = touchclickState;
+				touchclickState = null;
+				if (Math.abs(event.changedTouches[0].clientX - touchclickStateTmp.x) < 10 &&
+					Math.abs(event.changedTouches[0].clientY - touchclickStateTmp.y) < 10)
+					fn(event);
+			}
+			
 			$(this).each( function () {
-				this.addEventListener("touchcancel", newFn);
+				this.addEventListener("touchstart", downHandler);
+				this.addEventListener("touchend", upHandler);
 			});		
 		};	
 	}
