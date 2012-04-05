463980f6d0c6ac6e1a61e6e3efe598a05a9babc9
vasyaod <vasyaod@difur.ru>
2012-04-06 00:23:18 +0600

    Сделано вытягивающее выпадающее окно.
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- client/src/permguide-dropdownmenu-plugin.js
+++ client/src/permguide-dropdownmenu-plugin.js
@@ -26,18 +26,46 @@
 					});
 					this.position.top = -top;
 				},
-			
+				
+				resize: function() {
+					var separatorHeight =  $(state.element).parent().children(".dropDownMenuSeparator").height();
+					var contentHeight = $(state.element).find(".vScroller").height()+10;
+					var dropdownmenuHeight = (contentHeight+separatorHeight);
+					//alert(dropdownmenuHeight);
+					$(this.element).css("height", dropdownmenuHeight+"px");
+					$(this.element).find(".dropDownMenuContent").css("height", contentHeight+"px");
+					$(this.element).find(".toggleButton").css("height", separatorHeight+"px");
+					
+					// Вычисляем верхнюю позицию меню.
+					var top = dropdownmenuHeight - separatorHeight;
+					$(this.element).offset({
+						top: -top
+					});
+					this.position.top = -top;
+				},
+
+				refresh: function() {
+					var topPosition;
+					
+					if (this.closed) {
+						topPosition = this.position.top;
+						$(".dropDownMenuBg").css("display", "none");
+					} else {
+						topPosition = 0;
+						$(".dropDownMenuBg").css("display", "block");
+					}
+
+					$(this.element).animate({
+						top: topPosition
+					}, 500);
+				},
+				
 				close: function() {
 					if (this.closed)
 						return;
 	
 					this.closed = !this.closed;
-					var topPosition = this.position.top;
-	
-					$(".dropDownMenuBg").css("display", "none");
-					$(this.element).animate({
-						top: topPosition
-					}, 500);
+					this.refresh();
 				},
 			
 				open: function() {
@@ -45,19 +73,65 @@
 						return;
 	
 					this.closed = !this.closed;
-					var topPosition = 0;
-	
-					$(".dropDownMenuBg").css("display", "block");
-					$(this.element).animate({
-						top: topPosition
-					}, 500);
+					this.refresh();
 				},
 				
 				toggle: function() {
-					if (this.closed)
-						this.open();
-					else
-						this.close();
+					this.closed = !this.closed;
+					this.refresh();
+				},
+				
+				down: function(event) {
+					
+					this.x = event.changedTouches[0].clientX;
+					this.y = event.changedTouches[0].clientY;
+					this.containerPosition = $(this.element).offset();
+					
+					this.draged = true;
+					this.moved = false;
+					
+				},
+					
+				move: function(event) {
+					
+					if (!this.draged)
+						return;
+					var tX = event.changedTouches[0].clientX;
+					var tY = event.changedTouches[0].clientY;
+					// Если сдвиг не очень большой, то стоим на месте.
+					if (!this.moved && Math.abs(tY - this.y) < PermGuide.deadRadius)
+						return;
+					this.moved = true;
+					var top = this.containerPosition.top + (tY - this.y);
+					
+					if (top > 0)	// Окно не должно "отрываться" от верху.
+						return;
+					if (top < this.position.top)	// Окно не должно "уходить" верх больше чем нужно.
+						return;
+					
+					$(this.element).offset({ 
+						top: top
+					});
+				},
+				
+				up: function(event) {
+					if (!this.draged)
+						return;
+					this.draged = false;
+					
+					if (!this.moved)
+						return;
+					this.moved = false;
+					
+					var tX = event.changedTouches[0].clientX;
+					var tY = event.changedTouches[0].clientY;
+					
+					if (tY - this.y > 0 && Math.abs(tY - this.y) > $(this.element).height()*0.25)
+						this.closed = false;
+					else if (tY - this.y < 0 && Math.abs(tY - this.y) > $(this.element).height()*0.25)
+						this.closed = true;
+
+					this.refresh();
 				}
 			};
 
@@ -69,9 +143,10 @@
 			
 			
 			// Вешаем событие на кнопку сварачивания меню.
-			$(this).children(".toggleButton").touchclick( function (event){
-				state.toggle();
-			});
+			$(this).children(".toggleButton").touchstart($.proxy(state.down, state));
+			$(this).children(".toggleButton").touchmove($.proxy(state.move, state));
+			$(this).children(".toggleButton").touchend($.proxy(state.up, state));
+			$(this).children(".toggleButton").touchclick($.proxy(state.toggle, state));
 			
 			// Если произошел клик внутри меню, надо его задержать.
 			$(this).touchclick( function (event){
