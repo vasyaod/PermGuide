eada9d0735a0b76577b6a7c2a491ec7301b533f1
vasyaod <vasyaod@difur.ru>
2012-04-08 23:59:28 +0600

    Аудиогид функционирует отлично!
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- client/src/permguide-audio.js
+++ client/src/permguide-audio.js
@@ -3,44 +3,100 @@
 if(typeof PermGuide == "undefined")
 	PermGuide = {};
 
-PermGuide.Audio = {
-		
-	play: function(object) {
-		if (!this.audioElement)
-		{
-			this.audioElement = document.createElement('audio');
-			this.audioElement.onerror = $.proxy(function (){
-				this.stop();
-				alert("audio error: "+this.audioElement.error.code);
-			}, this);
-		}
-		
-		if (object.audio) {
-			var url = null;
-			if (this.audioElement.canPlayType) {
-
-				var canPlayMp3 = "" != this.audioElement.canPlayType('audio/mpeg');
-				var canPlayOgg = "" != this.audioElement.canPlayType('audio/ogg; codecs="vorbis"');
-				var fileName = object.audio;
-				if(canPlayOgg)
-					url = 'http://permguide.ru/audio/'+fileName+'.ogg';
-				else if (canPlayMp3)
-					url = 'http://permguide.ru/audio/'+fileName+'.mp3';
-			}
-			
+PermGuide.HTMLAudio = {
+	
+	_init: function() {
+		this.audioElement = document.createElement('audio');
+		this.audioElement.onerror = $.proxy(function (){
 			this.stop();
+			alert("HTML5 audio error: "+this.audioElement.error.code);
+		}, this);
+	},
+
+	_supportedFormat: function() {
+		if (this.audioElement.canPlayType) {
+			var canPlayMp3 = "" != this.audioElement.canPlayType('audio/mpeg');
+			var canPlayOgg = "" != this.audioElement.canPlayType('audio/ogg; codecs="vorbis"');
+			if (canPlayOgg)
+				return ".ogg";
+			if (canPlayMp3)
+				return ".mp3";
+		}
+		return null;
+	},
+		
+	_play: function(url) {
+		if (this.audioElement) {
+			this.audioElement.setAttribute('src', url);
+			this.audioElement.play();
+		}
+	},
+	
+	_stop: function() {
+		if (this.audioElement)
+			this.audioElement.pause();
+	}
+};
+
+PermGuide.PhonegapAudio = {
+		
+	_init: function() {
+	},
+
+	_supportedFormat: function() {
+		return ".mp3";
+	},
 			
-			if (url) {
-				this.audioElement.setAttribute('src', url);
-				this.audioElement.play();
-				return true;
-			}
+	_play: function(url) {
+		this.media = new Media(url);
+		this.media.play();
+	},
+		
+	_stop: function() {
+		if (this.media)
+		{
+			this.media.stop();
+			this.media.release();
+		}
+	}
+};
+
+PermGuide.Audio = {
+	
+	inited: false,
+	
+	init: function(object) {
+		if (this.inited)
+			return;
+		this.inited = true;
+		
+		// Если это фонегап то наследуемся от определенного объекта
+		if (PermGuide.isPhonegap) {
+			$.extend(this, PermGuide.PhonegapAudio);
+		} else {
+			$.extend(this, PermGuide.HTMLAudio);
+		}
+		this._init();
+	},
+
+	play: function(object) {
+		this.init();
+		this.stop();
+		var supportedFormat = this._supportedFormat();
+
+		if (supportedFormat && object.audio) {
+			var fileName = object.audio;
+			var url = 'http://permguide.ru/audio/'+fileName+supportedFormat;
+			
+			this._play(url);
+			return true;
 		}
 		return false;
 	},
-
+	
 	stop: function() {
+		this.init();
 		$(".sndPlay").removeClass("sndPlay");
-		this.audioElement.pause();
+		this._stop();
 	}
-}
+};
