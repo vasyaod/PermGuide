0518c68f99faf242460d776352cc7e771031d540
vasyaod <vasyaod@difur.ru>
2012-04-17 16:01:09 +0600

    Разделен код языковых ресурсов.
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- client/src/permguide-lr.js
+++ client/src/permguide-lr.js
@@ -3,10 +3,6 @@
 if(typeof PermGuide == "undefined")
 	PermGuide = {};
 
-String.prototype.trim = function () {
-	return this.replace(/^\s*/, "").replace(/\s*$/, "");
-}
-
 /**
  * Файл с языковыми ресурсами.
  */
@@ -123,79 +119,5 @@ PermGuide.lr = {
 	"kilometers": {
 		ru: "км",
 		en: "km"
-	}
-}
-/**
- * Небольшой плагин для интернационазизации интерфейса.
- */
-$.fn.i18n = function(props) {
-	this.each(function(index){
-		var text = PermGuide.Language.getInterfaceString($(this).text());
-		$(this).text(text);
-	});
-};
-
-/**
- * Объект отвечает за работу с мультиизычностью.
- */
-PermGuide.Language = {
-
-	// Флаг того, что пользователь выбрал язык.
-	languageSelected: false,
-	
-	// Пока язык задан статично.
-	currentLanguage: "ru",
-
-	init: function () {
-		var res = $.url.parse(document.URL);
-		if (res.params && res.params.lang) {
-			this.languageSelected = true;
-			this.currentLanguage = res.params.lang;
-		}
-	},
-	
-	/**
-	 * Метод устанавливает текущий язык.
-	 */
-	setCurrentLanguage: function (currentLanguage) {
-		this.currentLanguage = currentLanguage;
-		
-		//var res = $.url.parse(document.URL);
-		//res.source = null;
-		//if (!res.params)
-		//	res.params = {};
-		//res.params.lang = currentLanguage;
-		//window.location.href = $.url.build(res);
-	},
-	
-	/**
-	 * Возвращает строку на основании текущего(выбранного) языка.
-	 */
-	getString: function(object)
-	{
-		if (object == null)
-			return null;
-		
-		// Если объект уже является строкой, то возвразщаем строку.
-		if (typeof obj === "string")
-			return obj;
-			
-		if (this.currentLanguage == "ru" && object[this.currentLanguage] == null)
-			return object["rus"];
-		
-		return object[this.currentLanguage];
-	},
-
-	/**
-	 * Возвращает строку из языковых ресурсов в соответствии с выбранным 
-	 * языком.
-	 */
-	getInterfaceString: function(key)
-	{
-		var object = PermGuide.lr[key.trim()];
-		if (object == null)
-			return key;
-		
-		return this.getString(object);
 	}
 }
