bbbd010203740eb9b2b0b9bc68c61b9320a78a99
vasyaod <vasyaod@difur.ru>
2012-04-18 16:16:00 +0600

    Сайт с англиским текстом.
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- site/permguide-lr.js
+++ site/permguide-lr.js
@@ -13,27 +13,29 @@ PermGuide.lr = {
 		en: "Mobile<br/>Perm guide"
 	},
 	"text": {
-		ru: "Многие традиционные заметки про Пермь начинаются словами про административный, культурный, научный и производственный центр Западного Урала. Другие статьи рассказывают про пермское современное искусство и культурную революцию. А кто-то вообще черпает информацию о Перми из телесериалов. Так что же такое Пермь? Что здесь можно посмотреть интресного? И стоит ли сюда ехать?<br/><br/>Мы надеемся, что наше мобильное приложение поможет дать ответы на все эти вопросы, а кроме того, позволит любому путешественнику или жителю города пройтись по интересным местам, узнать их историю. Используя наушники и Мобильный гид, можно превратить обычную прогулку в увлекательное путешествие.<br/><br/>					Пермь стремительно меняется. Из пелены забвения возвращаются старые традиции и зарождаются абсолютно новые. Здесь Европа встречается с Азией, а современное искусство - с древними деревянными богами. Это город контрастов, интересный город, который заслуживает вашего внимания.	<br/><br/>	Вы можете ознакомиться с работой Мобильного гида прямо в браузере на сайте. Загрузить приложение можно или в Google Play, или прямо с сайта. В ближайшее время программа станет доступна для устройств iOS в Apple App Store. Также ведутся работы по портированию гида для Windows Phone 7 и Samsung Bada. На данный момент информация предоставляется на русском и английском языках. Для полноценной работы Мобильного гида требуется соединение с Интернет (к счастью, WiFi в Перми есть даже в трамваях, а 3G появился раньше, чем в Москве), хотя базовые функции работают и без него.	<br/><br/>	Поддержка приложения осуществляется через группы <a href='http://vk.com/permguide' target='_blank'>vkontakte</a> и <a href='http://facebook.com/permguide' target='_blank'>facebook</a>. Будем рады любым вашим замечаниям и предложениям. Приятного Вам путешествия!",
-		en: "Перевод скоро будет! Ж)"
-	},
+		ru: "Многие традиционные заметки про Пермь начинаются словами про административный, культурный, научный и производственный центр Западного Урала. Другие статьи рассказывают про пермское современное искусство и культурную революцию. А кто-то вообще черпает информацию о Перми из телесериалов. Так что же такое Пермь? Что здесь можно посмотреть интресного? И стоит ли сюда ехать?<br/><br/>Мы надеемся, что наше мобильное приложение поможет дать ответы на все эти вопросы, а кроме того, позволит любому путешественнику или жителю города пройтись по интересным местам, узнать их историю. Используя наушники и Мобильный гид, можно превратить обычную прогулку в увлекательное путешествие.",
+		en: "Many articles about Perm begins from telling about administrative, cultural, scientific and industrial center of the Western Ural. Other articles tell about the Perm modern art and a cultural revolution. So what is Perm?<br/><br/> What is there interesting to look at? Whether is it necessary to travel here?<br/><br/> We hope that our mobile application will help everybody to have answers to all these questions. And also it will allow travelers and Perm residents to walk on interesting places and to learn their history. Just use your earphones and the Mobile guide and an usual walk will turn into a fascinating travel."},
+	"text2": {
+		ru: "Пермь стремительно меняется. Из пелены забвения возвращаются старые традиции и зарождаются абсолютно новые. Здесь Европа встречается с Азией, а современное искусство - с древними деревянными богами. Это город контрастов, интересный город, который заслуживает вашего внимания.	<br/><br/>	Вы можете ознакомиться с работой Мобильного гида прямо в браузере на сайте. Загрузить приложение можно или в Google Play, или прямо с сайта. В ближайшее время программа станет доступна для устройств iOS в Apple App Store. Также ведутся работы по портированию гида для Windows Phone 7 и Samsung Bada. На данный момент информация предоставляется на русском и английском языках. Для полноценной работы Мобильного гида требуется соединение с Интернет (к счастью, WiFi в Перми есть даже в трамваях, а 3G появился раньше, чем в Москве), хотя базовые функции работают и без него.	<br/><br/>	Поддержка приложения осуществляется через группы <a href='http://vk.com/permguide' target='_blank'>vkontakte</a> и <a href='http://facebook.com/permguide' target='_blank'>facebook</a>. Будем рады любым вашим замечаниям и предложениям. Приятного Вам путешествия!",
+		en: "Here Europe meets Asia, and the modern art meets ancient wooden gods. It is a city of the contrasts, a very interesting city which deserves your attention. <br/><br/>You can see the Mobile guide directly in the browser on a site. And you can download the application from Google Play or directly from the site. It is coming soon in Apple App Store and other mobile platforms. Now information is provided in Russian and English.<br/><br/>You can chat with the developers in our <a href='http://facebook.com/permguide' target='_ blank'> facebook group</a>."},
 	"Приложение в Google Play": {
 		ru: "Приложение в Google Play",
-		en: "Приложение в Google Play"
+		en: "Google Play"
 	},
 	"Загрузить приложение под Android с сайта": {
 		ru: "Загрузить приложение под Android с сайта",
-		en: "Загрузить приложение под Android с сайта"
+		en: "Download now"
 	},
 	"Группа в ВКонтакте": {
 		ru: "Группа в ВКонтакте",
-		en: "Группа в ВКонтакте"
+		en: "vk.com"
 	},
 	"Приложение в ВКонтакте": {
 		ru: "Приложение в ВКонтакте",
-		en: "Приложение в ВКонтакте"
+		en: "Application in vk.com"
 	},
 	"Страница в facebook": {
 		ru: "Страница в facebook",
-		en: "Страница в facebook"
+		en: "Facebook"
 	}
 }
