975b84b9c909f454e11e07d4baac5028d90112e7
vasyaod <vasyaod@difur.ru>
2012-04-24 14:37:45 +0600

    Изменения в нативном приложении, передача параметров в приложение через URL.
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- client/android/PermGuide/src/ru/mobak/permguide/PermGuide.java
+++ client/android/PermGuide/src/ru/mobak/permguide/PermGuide.java
@@ -1,9 +1,11 @@
 package ru.mobak.permguide;
 
 
+import java.util.Locale;
+
 import android.os.Bundle; 
 
-import com.phonegap.*;                                                                                                        
+import com.phonegap.*;
 
 public class PermGuide extends DroidGap
 {
@@ -12,6 +14,19 @@ public class PermGuide extends DroidGap
 	{
 		super.onCreate(savedInstanceState);
 		super.setIntegerProperty("loadUrlTimeoutValue", 40000);
-		super.loadUrl("file:///android_asset/www/index.html");
+		
+		String langParam = "";
+		if (Locale.getDefault().getLanguage().equals("ru"))
+			langParam = "&lang=ru";
+			
+		// В 13 и 14 версия sdk есть касяк который не дает передовать параметры
+		// в дщкальный урл. Например: file://index.html?p=1 выдает ошибку. 
+		// Этот кастыль какраз сделан что бы избежать этой ошибки. 
+		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+		if (currentapiVersion >= 13){
+			super.loadUrl("file:///android_asset/www/index.html");
+		} else {
+			super.loadUrl("file:///android_asset/www/index.html?phonegap=1"+langParam);
+		}
 	}
 }
